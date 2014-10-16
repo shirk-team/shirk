84fb71e3eb1ca000e8bd3ffa2df0a3272ed492c2
Lily Seropian <lilyseropian@gmail.com>
2014-10-16 17:05:45 -0400

    Put more things in DB

--- tests/TEST.md
+++ tests/TEST.md
@@ -4,7 +4,7 @@
 ### Testing directions
 
 1. Create the folder /tests/test_db (this folder is git-ignored).
-2. Run <code>mongod --dbpath /tests/test_db.</code>.
+2. Run <code>mongod --dbpath /tests/test_db</code>.
 3. Run <code>node /tests/seed_db.js</code> to seed the test database.
 
 If you need to clear the test database, you can run 
--- tests/seed_db.js
+++ tests/seed_db.js
@@ -1,9 +1,10 @@
 /**
  * Script to seed a sample database for testing purposes. See TEST.md for
- * details about usage.
+ * details about usage. Throws any errors it encounters.
  * @author seropian@mit.edu
  */
 
+var User = require('../models/user.js').User;
 var List = require('../models/list.js').List;
 var Task = require('../models/task.js').Task;
 var async = require('async');
@@ -20,9 +21,35 @@ db.once('open', function () {
 });
 
 function seedDb() {
+    addUsers(function(err) {
+        if (err) throw err;
+
+        addHomework(function(err) {
+            if (err) throw err;
+
+            addHousework(function(err) {
+                if (err) throw err;
+                mongoose.connection.close();
+            });
+        });
+    });
+}
+
+function addUsers(callback) {
+    var users = [
+        new User({username: 'admin', password: 'admin'}),
+        new User({username: 'andre', password: 'andre'}),
+        new User({username: 'lily', password: 'lily'}),
+        new User({username: 'tricia', password: 'tricia'}),
+    ];
+
+    saveThings(users, callback);
+}
+
+function addHomework(callback) {
     var homework = new List({title: 'Homework'});
     homework.save(function(err) {
-        if (err) return console.error.bind(console, 'connection error:');
+        if (err) throw err;
 
         var tasks = [
             new Task({
@@ -54,24 +81,55 @@ function seedDb() {
             })
         ];
        
-        async.each(tasks,
-            function(task, callback) {
-                task.save(function(err) {
-                    if (err) {
-                        callback(console.error.bind(console, 'connection error:'));
-                    } else {
-                        callback(null);
-                    }
-                });
-            },
-            function(err) {
-                if (err) {
-                    console.log('The following error occured while seeding ' +
-                        'the database. Please clear the database and try ' + 
-                        'again.\n', err);
-                }
-                mongoose.connection.close();
-            }
-        );
-    });        
+        saveThings(tasks, callback);
+    });   
+}
+
+function addHousework(callback) {
+    var housework = new List({title: 'Housework'});
+    housework.save(function(err) {
+        if (err) throw err;
+
+        var tasks = [
+            new Task({
+                title: 'Clean room!',
+                list: housework,
+                priority: 1,
+                notes: 'Open the windows to air it out xP',
+                deadline: new Date("September 1, 2014")
+            }),
+            new Task({
+                title: 'Do laundry',
+                list: housework,
+                completed: false,
+                notes: '+ ironing',
+                deadline: new Date("August 3, 2012")
+            }),
+            new Task({
+                title: 'Wash the dishes',
+                list: housework,
+                completed: false,
+                notes: 'before roomate\'s parents come visit!!!',
+                priority: 1
+            }),
+            new Task({
+                title: 'Organize sock drawer',
+                list: housework,
+                completed: true,
+                priority: -1,
+                deadline: new Date("August 18, 2015")
+            })
+        ];
+
+        saveThings(tasks, callback);
+    })
+}
+
+var saveThings = function(things, callback) {
+    async.each(things,
+        function(thing, callback) {
+            thing.save(callback);
+        },
+        callback
+    );
 }
