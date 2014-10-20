617dbcfac73b5781f981a4e3b6b2cc0e7763afe2
Lily Seropian <lilyseropian@gmail.com>
2014-10-20 15:31:06 -0400

    Delete test working but messy

--- tests/tests_lists.js
+++ tests/tests_lists.js
@@ -3,23 +3,45 @@
 * @author aandre@mit.edu
 */
 
-function login (username, password, title, callback) {
+function runTest (username, password, title, test, nextTest) {
     $.ajax({
         url : '/login',
         type: 'POST',
         data : {username: username, password: password},
         success: function() {
-            callback(title);
+            test(title, nextTest);
         },
         error: function() {
             //TODO: change to throw error once we figure out login stuff
-            callback(title);
+            test(title, nextTest);
             // QUnit.test(title + ' login failed', function(assert) {});
         }
     });
 };
 
-login('admin', 'admin', 'List - GET /lists/', function(title) {
+
+/**
+ * THIS MUST BE RUN LAST BECAUSE IT DELETES THINGS FROM THE DB
+ */
+var testDelete = function(id) {
+    runTest('admin', 'admin', 'List - DELETE /lists/', function(title) {
+        $.ajax({
+            url : '/lists/' + id,
+            type: 'DELETE',
+            async: false,
+            success: function (data, textStatus, jqXHR) {
+                QUnit.test(title, function(assert) {
+                    assert.equal(data.listID, id);
+                });
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                QUnit.test(title + ' request failed', function(assert) {});
+            }
+        });
+    });
+};
+
+runTest('admin', 'admin', 'List - GET /lists/', function(title, nextTest) {
     $.ajax({
         url : '/lists/',
         type: 'GET',
@@ -29,10 +51,11 @@ login('admin', 'admin', 'List - GET /lists/', function(title) {
                 lists = data.lists;
                 assert.equal(lists.length, 1, 'Correct number of lists returned.');
                 assert.equal(lists[0].title, 'Homework', 'Correct list returned.');
+                nextTest(lists[0]._id);
             });
         },
         error: function(jqXHR, textStatus, errorThrown) {
             QUnit.test(title + ' request failed', function(assert) {});
         }
     });
-});
+}, testDelete);
