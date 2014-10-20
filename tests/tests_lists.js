74fa83d78213926bba577952a632f84045a3d4d1
Lily Seropian <lilyseropian@gmail.com>
2014-10-20 16:32:53 -0400

    Made test file cleaner

--- tests/tests_lists.js
+++ tests/tests_lists.js
@@ -1,61 +1,58 @@
 /**
 * Automated tests for Lists.
-* @author aandre@mit.edu
+* @author aandre@mit.edu, seropian@mit.edu
 */
 
-function runTest (username, password, title, test, nextTest) {
+$.ajax({
+    url : '/login',
+    type: 'POST',
+    data : {
+        username: 'admin',
+        password: 'admin'
+    },
+    success: function() {
+        testGet();
+    },
+    error: function() {
+        //TODO: change to throw error once we figure out login stuff
+        testGet();
+        // QUnit.test(title + ' login failed', function(assert) {});
+    }
+});
+
+function testMethod(title, url, method, onSuccess) {
     $.ajax({
-        url : '/login',
-        type: 'POST',
-        data : {username: username, password: password},
-        success: function() {
-            test(title, nextTest);
-        },
-        error: function() {
-            //TODO: change to throw error once we figure out login stuff
-            test(title, nextTest);
-            // QUnit.test(title + ' login failed', function(assert) {});
-        }
-    });
-};
-
-
-/**
- * THIS MUST BE RUN LAST BECAUSE IT DELETES THINGS FROM THE DB
- */
-var testDelete = function(id) {
-    runTest('admin', 'admin', 'List - DELETE /lists/', function(title) {
-        $.ajax({
-            url : '/lists/' + id,
-            type: 'DELETE',
-            async: false,
-            success: function (data, textStatus, jqXHR) {
-                QUnit.test(title, function(assert) {
-                    assert.equal(data.listID, id);
-                });
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                QUnit.test(title + ' request failed', function(assert) {});
-            }
-        });
-    });
-};
-
-runTest('admin', 'admin', 'List - GET /lists/', function(title, nextTest) {
-    $.ajax({
-        url : '/lists/',
-        type: 'GET',
+        url : url,
+        type: method,
         async: false,
-        success: function (data, textStatus, jqXHR) {
-            QUnit.test(title, function(assert) {
-                lists = data.lists;
-                assert.equal(lists.length, 1, 'Correct number of lists returned.');
-                assert.equal(lists[0].title, 'Homework', 'Correct list returned.');
-                nextTest(lists[0]._id);
-            });
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
+        success: onSuccess,
+        error: function() {
             QUnit.test(title + ' request failed', function(assert) {});
         }
     });
-}, testDelete);
+}
+
+function testGet() {
+    var title = 'List - GET /lists/';
+
+    testMethod(title, '/lists/', 'GET', function(data) {
+        QUnit.test(title, function(assert) {
+            lists = data.lists;
+            assert.equal(lists.length, 1, 'Correct number of lists returned.');
+            assert.equal(lists[0].title, 'Homework', 'Correct list returned.');
+
+            testDelete(lists[0]._id);            
+        });
+    });
+}
+
+function testDelete(id) {
+    var title = 'List - DELETE Homework list';
+
+    testMethod(title, '/lists/' + id, 'DELETE', function (data) {
+            QUnit.test(title, function(assert) {
+                assert.equal(data.listID, id);
+            });
+        }
+    );
+}
