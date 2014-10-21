b1ccf18b85b46c52a048372c069072e9053d86b3
Lily Seropian <lilyseropian@gmail.com>
2014-10-21 16:43:03 -0400

    Wrote delete task test and made it work

--- tests/tests_tasks.js
+++ tests/tests_tasks.js
@@ -63,11 +63,34 @@ test('Task - DELETE /tasks/:id', function() {
     login ('test1', 'test1');
     clear_all();
 
+    // Create List
+    var list = list_create('TestList1').list;
+
     // Create Two Tasks
+    var task1 = task_create({
+        title: 'TestTask1',
+        list: list._id,
+        notes: 'This is a test task.'
+    }).task;
+
+    var task2 = task_create({
+        title: 'TestTask2',
+        list: list._id,
+        notes: ''
+    }).task;
 
     // Verify Tasks
-
+    deepEqual(task_get(task1._id).task, task1, 
+        'Task ' + task1._id.toString() + ' successfully created.');
+    deepEqual(task_get(task2._id).task, task2, 
+        'Task ' + task2._id.toString() + ' successfully created.');
+    
     // Delete Task 1
+    task_delete(task1._id);
 
     // Verify Deletion
+    equal(task_get(task1._id), null, 
+        'Task ' + task1._id.toString() + ' successfully deleted.');
+    deepEqual(task_get(task2._id).task, task2, 
+        'Task ' + task2._id.toString() + ' not deleted (unaffected).');
 });
