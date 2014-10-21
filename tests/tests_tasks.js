95b44fdae057f47a338cad988fe8b764a9959c28
Tricia Divita <tdivita@mit.edu>
2014-10-21 17:06:11 -0400

    Worked on task creation test. Is not passing, so debugging now.

--- tests/tests_tasks.js
+++ tests/tests_tasks.js
@@ -30,12 +30,33 @@ test('Task - GET /tasks/:id', function () {
 ///////////////
 
 test('Task - POST /tasks/', function () {
+	// TODO: Finish
     login('test1', 'test1');
     clear_all();
 
+    // Create a List to Hold the Tasks
+    var list1 = list_create("ListOne").list;
+
     // Create Tasks
+    // title(req), list(req), completed(default=false), priority(default=0), notes, deadline, owner(req)
+    var task1_content = {
+    	title: "Task One",
+    	list: list1._id
+    };
+
+    var task1 = task_create(task1_content); //.task;
+    console.log(task1);
 
     // Verify Tasks
+    deepEqual(task_get(task1._id).task, task1,
+        "Task "+ task1._id.toString() + " successfully created.");
+    // deepEqual(task_get(task2._id).task, task2,
+    //     "Task "+ task2._id.toString() + " successfully created.");
+    // deepEqual(task_get(task3._id).task, task3,
+    //     "Task "+ task3._id.toString() + " successfully created.");
+    // // Cannot create a list with an empty title, so this should have failed.
+    // equal(undefined, task4,
+    //     "Task not successfully created. Cannot have empty title.");
 });
 
 /////////////////////
