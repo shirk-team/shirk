ccdc7d8c16d310fdd9998b2aed97c8d259615afe
Lily Seropian <lilyseropian@gmail.com>
2014-10-24 22:00:44 -0400

    Reduced repeated code

--- public/js/login.js
+++ public/js/login.js
@@ -17,7 +17,6 @@ $(document).ready(function() {
             } else {
                 login(username, password)
             }
-            // TODO: ajax
         }
     });
 });
@@ -28,53 +27,73 @@ $(document).ready(function() {
  * @return {String} The current value of the field.
  */
 function checkField(field) {
-    var elt = '#' + field;
-    var input = $(elt + '-input').val();
+    var input = $('#' + field + '-input').val();
     if (input  === '') {
-        $(elt).addClass('error');
-        $(elt).append('<div class="ui red pointing label">' + field +
-            ' Required</div>');
+        showError(field, field + ' required');
     }
     return input;
 }
 
-function signup(username, password) {
+/**
+ * Make a user-related AJAX POST request (login or signup). If the request
+ * is successful, replaces the page's contents with the page returned by the
+ * server.
+ * @param  {String} username  The username of the user affected by the request.
+ * @param  {String} password  The password of the user affected by the request.
+ * @param  {String} url       The URL to post to.
+ * @param  {Function} onError A handler for if the server returns an error.
+ */
+function userAJAX(username, password, url, onError) {
     $.ajax({
-        url: '/users/',
+        url: url,
         data: {
             username: username,
             password: password
         },
         success: overwritePage,
-        error: function(xhr, status, error) {
-            if (xhr.responseText.indexOf('dup key') !== -1) {
-                $('#username').addClass('error');
-                $('#username').append('<div class="ui red pointing label">Username Taken</div>');
-            }
-        },
+        error: onError,
         type: 'POST'
     });
 }
-
-function login(username, password) {
-    $.ajax({
-        url: '/login',
-        data: {
-            username: username,
-            password: password
-        },
-        success: overwritePage,
-        error: function(xhr, status, error) {
-            if (xhr.responseText === 'Incorrect username') {
-                $('#username').addClass('error');
-                $('#username').append('<div class="ui red pointing label">Incorrect Username</div>');
-            } else if (xhr.responseText === 'Incorrect password') {
-                $('#password').addClass('error');
-                $('#password').append('<div class="ui red pointing label">Incorrect Password</div>');
-            } else {
-                throw new Error(xhr.responseText);
-            }
-        },
-        type: 'POST'
+/**
+ * Create a new user account. If the username is already taken, show an
+ * error message. Otherwise, log the user in.
+ * @param  {String} username The username of the new account.
+ * @param  {String} password The password for the new account.
+ */
+function signup(username, password) {
+    userAJAX(username, password, '/users/', function(xhr, status, error) {
+        if (xhr.responseText.indexOf('dup key') !== -1) {
+            showError('username', 'username taken');
+        }
     });
+}
+
+/**
+ * Log the user in. If the username or password are incorrect, show an error
+ * message.
+ * @param  {String} username The username of the account to log into.
+ * @param  {String} password The password for the account to log into.
+ */
+function login(username, password) {
+    userAJAX(username, password, '/login', function(xhr, status, error) {
+        if (xhr.responseText === 'Incorrect username') {
+            showError('username', 'incorrect username');
+        } else if (xhr.responseText === 'Incorrect password') {
+            showError('password', 'incorrect password');
+        } else {
+            throw new Error(xhr.responseText);
+        }
+    });
+}
+
+/**
+ * Show an error message on an input field.
+ * @param  {String} field   The id of the field to show the error on.
+ * @param  {String} message The error message to show.
+ */
+function showError(field, message) {
+    var elt = $('#' + field);
+    $(elt).addClass('error');
+    $(elt).append('<div class="ui red pointing label">' + message + '</div>');
 }
