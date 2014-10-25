236728b7f0e8d18c7d7c071496f5be71df07e073
Lily Seropian <lilyseropian@gmail.com>
2014-10-24 21:07:43 -0400

    Added UI error for empty fields

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-    
+    $('.button').click(function() {
+        // Clear errors from previous login/signup attempts
+        $('.ui.red.pointing.label').remove();
+        $('.error').removeClass('error');
+
+        var username = checkField('username');
+        var password = checkField('password');
+
+        if (username && password) {
+            // TODO: ajax
+        }
+    });
 });
+
+function checkField(field) {
+    var elt = '#' + field;
+    var input = $(elt + '-input').val();
+    if (input  === '') {
+        $(elt).addClass('error');
+        $(elt).append('<div class="ui red pointing label">' + field + ' Required</div>');
+    }
+    return input;
+}
