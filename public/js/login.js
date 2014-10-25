e038996482e07405791e5038ed030a4b9dbda1d8
Lily Seropian <lilyseropian@gmail.com>
2014-10-24 21:09:49 -0400

    Comments

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    /**
+     * Check if all required fields are filled out. If so, send a login/signup
+     * request to the server, depending on which button was clicked.
+     */
     $('.button').click(function() {
         // Clear errors from previous login/signup attempts
         $('.ui.red.pointing.label').remove();
@@ -13,12 +17,18 @@ $(document).ready(function() {
     });
 });
 
+/**
+ * Check if a required field has been provided.
+ * @param  {String} field The name and id of the field to check.
+ * @return {String} The current value of the field.
+ */
 function checkField(field) {
     var elt = '#' + field;
     var input = $(elt + '-input').val();
     if (input  === '') {
         $(elt).addClass('error');
-        $(elt).append('<div class="ui red pointing label">' + field + ' Required</div>');
+        $(elt).append('<div class="ui red pointing label">' + field +
+            ' Required</div>');
     }
     return input;
 }
